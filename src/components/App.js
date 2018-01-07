7a9c51959fa1e4cd06c84a686eb4fd57680b2378
Alfredo <atakori@outlook.com>
2018-01-07 17:23:30 -0500

    added dashboard component and route

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
 import {BrowserRouter as Router, Route, Switch, Redirect} from 'react-router-dom';
+
 import {LandingPage} from './home';
 import {Login} from './login'
 import {SignUp} from './signup'
+import {Dashboard} from './dashboard'
 
 class App extends Component {
   render() {
@@ -13,6 +15,7 @@ class App extends Component {
 	      		<Route exact path= '/' component= {LandingPage} />
 	      		<Route exact path= '/login' component= {Login}/>
 	      		<Route exact path= '/signup' component= {SignUp}/>
+	    		<Route exact path= '/dashboard' component= {Dashboard}/>
 	    	</Switch>
 	    </Router>
     );
---
+++ src/components/dashboard.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export function Dashboard(props) {
+	return (
+		<p> This is the Dashboard Page </p>
+		)
+}
