abd0006afbc033a0b03fabec389d3b44032adc4b
Alfredo <atakori@outlook.com>
2018-01-10 23:25:48 -0500

    added form to signup.js

--- src/components/login.js
+++ src/components/login.js
@@ -1,12 +1,24 @@
 import React from 'react';
-import Button from 'antd/lib/button'; 
+import {NavigationBar} from './navbar';
 
 export function Login(props) {
 	return (
-		<div className= "login_page">
-			<Button type="primary">Button</Button>
-			<button className= "btn btn-primary"> button</button>
-			<p> This is the Login Page</p>
-		</div>
+		<section className= "login_section">
+			<header role= "banner">
+				<NavigationBar />
+			</header>
+			<main role= "main"> 
+				<div className= "login_content" style= {{paddingTop: "65px"}}>
+					<form className= "login_form">
+						<h1 className= "form_title">Sign Up</h1>
+						<label for = "username">Username </label>
+						<input type= "text" name="username" id="username" />
+						<label for= "password">Password </label>
+						<input type= "password" name="password" id="password"/>
+						<button type= "submit">Sign Up </button>
+					</form>
+				</div>
+			</main>
+		</section>
 		)
 }
--- src/components/signup.js
+++ src/components/signup.js
@@ -11,16 +11,16 @@ export function SignUp(props) {
 				<div className= "signup_content" style= {{paddingTop: "65px"}}>
 					<form className= "sign_up_form">
 						<h1 className= "form_title">Sign Up</h1>
-						<label name= "first_name" id="first_name">First Name </label>
-						<input type= "text" name="first_name" />
-						<label name= "last_name" id="last_name">Last Name </label>
-						<input type= "text" name="last_name" />
-						<label name= "user_name" id="username">Username </label>
-						<input type= "text" name="username" />
-						<label name= "password" id="password">Password </label>
-						<input type= "password" name="password" />
-						<label name= "confirm_pass" id="confirm_pass">Confirm Password </label>
-						<input type= "password" name="confirm_pass" />
+						<label for= "first_name" >First Name </label>
+						<input type= "text" name="first_name" id="first_name"/>
+						<label for= "last_name" >Last Name </label>
+						<input type= "text" name="last_name" id="last_name"/>
+						<label for= "user_name" >Username </label>
+						<input type= "text" name="username" id="username"/>
+						<label for= "password" >Password </label>
+						<input type= "password" name="password" id="password"/>
+						<label for= "confirm_pass" >Confirm Password </label>
+						<input type= "password" name="confirm_pass" id="confirm_pass"/>
 						<button type= "submit">Sign Up </button>
 					</form>
 				</div>
