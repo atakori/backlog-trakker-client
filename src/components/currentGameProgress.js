d037181987710ec99495c09779fa8332c220128e
Alfredo <atakori@outlook.com>
2018-01-11 00:53:01 -0500

    added currentGameProgress.js

---
+++ src/components/currentGameProgress.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export class CurrentGameProgress extends React.Component{
+	constructor(props) {
+			super(props)
+		}
+	render() {
+		return (
+				<div> 
+					<p> appears</p>
+				</div>
+			)
+	}
+}
--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {LoggedInNav} from './loggedNav';
+import {CurrentGameProgress} from './currentGameProgress';
 
 
 export function Dashboard(props) {
@@ -8,8 +9,11 @@ export function Dashboard(props) {
 			<header role= "banner">
 				<LoggedInNav />
 			</header>
-			<main role="main">
-			<p> hello </p>
+			<main role="main" style= {{paddingTop: "65px"}}>
+				<CurrentGameProgress user= "test user 3" currentGame= "Dark Souls"/>
+			<div>
+				<p> hello </p>
+			</div>
 			</main>
 		</section>
 		)
