35c60f753a0856f0b38db9b41e8cf06b546df842
Alfredo <atakori@outlook.com>
2018-01-10 22:33:34 -0500

    added button to home.js

--- src/components/home.js
+++ src/components/home.js
@@ -8,24 +8,25 @@ export function LandingPage(props){
 				<NavigationBar />
 			</header>
 			<main role= "main">
-			<div className= "container title_section" style= {{paddingTop: "65px"}}> 
-				<h1 className= "main_title">Checkpoint </h1>
-				<p className= "subtitle"> Tackle that backlog</p>
-			</div>
-			<div className= "more_info info_one">
-				<h2 className= "title"> Too many unfinished games?</h2>
-				<p className= "details"> Checkpoint helps you keep track of your video game backlog by allowing you to mark your progress for each game that you started and how close you are to completing them.</p>
-			</div>
-			<div className= "more_info info_two">
-				<h2 className= "title"> Build Your Collection?</h2>
-				<p className= "image"> [Screenshot or image here]</p>
-				<p className= "details"> Search for new games to add to your game library and view game information to help you decide if you should add just one more awesome game to your backlog</p>
-			</div>
-			<div className= "more_info info_three">
-				<h2 className= "title"> Too many unfinished games?</h2>
-				<p className= "image"> [Screenshot or image here]</p>
-				<p className= "details"> Backlog Trakker helps you keep track of your video game backlog by allowing you to mark your progress for each game that you started and how close you are to completing them.</p>
-			</div>
+				<div className= "container title_section" style= {{paddingTop: "65px"}}> 
+					<h1 className= "main_title">Checkpoint </h1>
+					<p className= "subtitle"> Tackle that backlog</p>
+				</div>
+				<div className= "more_info info_one">
+					<h2 className= "title"> Too many unfinished games?</h2>
+					<p className= "details"> Checkpoint helps you keep track of your video game backlog by allowing you to mark your progress for each game that you started and how close you are to completing them.</p>
+				</div>
+				<div className= "more_info info_two">
+					<h2 className= "title"> Build Your Collection?</h2>
+					<p className= "image"> [Screenshot or image here]</p>
+					<p className= "details"> Search for new games to add to your game library and view game information to help you decide if you should add just one more awesome game to your backlog</p>
+				</div>
+				<div className= "more_info info_three">
+					<h2 className= "title"> Too many unfinished games?</h2>
+					<p className= "image"> [Screenshot or image here]</p>
+					<p className= "details"> Backlog Trakker helps you keep track of your video game backlog by allowing you to mark your progress for each game that you started and how close you are to completing them.</p>
+				</div>
+				<button className= "btn btn-primary">Tackle that Backlog </button>
 			</main>
 		</section>
 		)
