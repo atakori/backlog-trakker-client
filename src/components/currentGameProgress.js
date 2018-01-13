4b61626a4beedfa6a2eaf9d22ae4d2048ed6b11d
Alfredo <atakori@outlook.com>
2018-01-13 18:08:45 -0500

    completed mock info for CurrentGameProgress

--- src/components/currentGameProgress.js
+++ src/components/currentGameProgress.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import { Progress } from 'antd'
 
 export class CurrentGameProgress extends React.Component{
+	constructor (props) {
+		super(props)
+	}
+
 	render() {
 		return (
-				<div> 
-					<p> appears</p>
+				<div classNameName= "main_dashboard"> 
+					<h2 classNameName= "welcome_title"> {this.props.user}'s Backlog </h2>
+		          <h2 classNameName= "game_title"> Current Game: {this.props.currentGame} </h2>
+		          <img className= "game_box_art" src= {this.props.gameArtURL} alt="game_image" height= "30%" width="15%"/>
+		          <div className= "progress_stats">
+		          	<p> Perecentage complete</p>
+		          	<Progress type= "circle" percent= {this.props.progress}/>
+		          </div>
+		          <div className= "review_scores">
+		          	<p className= "critic_scores"> Avg. critic rating </p>
+		          	<Progress type= "dashboard" percent= {this.props.criticRating * 10} format={percent => `${percent / 10}`} />
+			        <p className= "user_avg_score">Avg. gamer rating </p>
+		          	<Progress type= "dashboard" percent= {this.props.userRating * 10} format={percent => `${percent / 10}`} />
+		          </div>
 				</div>
 			)
 	}
--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -10,8 +10,7 @@ export function Dashboard(props) {
 				<LoggedInNav />
 			</header>
 			<main role="main" style= {{paddingTop: "65px"}}>
-				<CurrentGameProgress user= "test user 3" currentGame= "Dark Souls"/>
-			<Progress type= "circle" percent= "50"/>
+				<CurrentGameProgress user= "gamerX_954" currentGame= "Dark Souls 3" progress= "78" criticRating= "7.4" userRating= "9.3" gameArtURL= "http://www.justpushstart.com/wp-content/uploads/2015/12/Dark-Souls-3-Box-Art-Day-One.jpg"/>
 
 			</main>
 		</section>
