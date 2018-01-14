691d106a992436180ad58a2c5f62936eafbf63e6
Alfredo <atakori@outlook.com>
2018-01-13 21:40:48 -0500

    added id for mockchapters

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -11,7 +11,7 @@ export function Dashboard(props) {
 			</header>
 			<main role="main" style= {{paddingTop: "65px"}}>
 				<CurrentGameProgress user= "gamerX_954" currentGame= "Dark Souls 3" progress= "78" criticRating= "7.4" userRating= "9.3" gameArtURL= "http://www.justpushstart.com/wp-content/uploads/2015/12/Dark-Souls-3-Box-Art-Day-One.jpg"/>
-				<CurrentGameChapters currentGame= "Dark Souls 3"/>
+				<CurrentGameChapters currentGame= "Dark Souls 3" gameChapters= {['Cemetery of Ash', 'Firelink Shrine', 'High Wall of Lothric', "Undead Settlement", "Road of Sacrifices", "Cathedral of the Deep", "Farron Keep"]}/>
 			</main>
 		</section>
 		)
--- src/components/gameChapters.js
+++ src/components/gameChapters.js
@@ -5,10 +5,35 @@ export class CurrentGameChapters extends React.Component {
 		super(props)
 	}
 
+/*	testGameChapters() {
+		let gameName = "dark-souls-3"
+
+		let url= "https://allorigins.me/get?url=" + encodeURIComponent(`www.google.com`) + "&callback=?";
+		$.getJSON('http://www.whateverorigin.org/get?url=' + encodeURIComponent('http://google.com') + '&callback=?', function(data){
+	alert(data.contents);
+});
+	// fetch method doesn't work | only Jquery works momentariliy
+		axios.get(url).then(function(res) {
+			alert(res);
+		})
+	}*/
+
 	getGameChapters() { 
-		let mockgamechapters= ['Cemetery of Ash', 'Firelink Shrine', 'High Wall of Lothric', "Undead Settlement", "Road of Sacrifices", "Cathedral of the Deep", "Farron Keep"];
-		let list = mockgamechapters.map(chapter => <li><input type= "checkbox" name= "game_chapter" />{chapter}</li>);
-		return list
+		//change this to get the chapters from the props
+
+		let game= this.props.currentGame
+
+		let mockchapters= ['Cemetery of Ash', "Firelink Shrine", "High Wall of Lothric", "Undead Settlement", "Road of Sacrifices", "Cathedral of the Deep", "Farron Keep"];
+		let list = mockchapters.map((chapter,index) => (
+			<li>
+				<input id={chapter} type="checkbox" name="game_chapter" onChange={this.handleChange} /><label for={chapter}>{chapter}</label>
+			</li>))
+		return list;
+	}
+
+	handleChange() {
+		//make the text strikethrough
+		console.log("works")
 	}
 
 	render() {
