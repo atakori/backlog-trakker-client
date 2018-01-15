58d38aaf8f28d215259522bedb81d3458d69a0e0
Alfredo <atakori@outlook.com>
2018-01-14 20:29:05 -0500

    added userBacklog.js

--- src/components/gameCollection.js
+++ src/components/gameCollection.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import { LoggedInNav } from './loggedNav'
 
 export function GameCollection(props) {
 	return (
-		<p> This is the Game Collection Page </p>
+		<section class= "game_collection_section"> 
+			<header role= "banner">
+				<LoggedInNav />
+			</header>
+			<main role= "main">
+	        	<h2 class= "collection_title"> My Game Collection</h2>
+				<p> This is the Game Collection Page </p>
+			</main>
+		</section>
 		)
 }
