e1d08ee3378bbf48deba80ef4ec9a5f6a22f7374
Alfredo <atakori@outlook.com>
2018-01-15 18:14:54 -0500

    added gameCollectionStatus() in gamePage.js

--- src/components/gamePage.js
+++ src/components/gamePage.js
@@ -7,6 +7,20 @@ export class GamePage extends React.Component {
 		super(props)
 	}
 
+	gameCollectionStatus() {
+		//checks for whether or not the game is in the users
+		//collection or not
+		let gameInCollection= false;
+		//this should go through the db to check and render
+		//a true or false value
+		if(gameInCollection) {
+			return (<button className= "btn btn-primary in_collection_button"> In Collection</button>
+)
+		} else { return (
+			<button className= "btn btn-primary add_button"> Add to Game Collection</button>
+			)
+		}
+	}
 
 	render() {
 	return (
@@ -18,6 +32,9 @@ export class GamePage extends React.Component {
 				<div className= "game_information">
 					<GameInfo gameName= {this.props.match.params.game}/>
 				</div>
+				<div className= "add_game_to_collection_section">
+           			{this.gameCollectionStatus()}
+        		</div>
 			</main>
 		</section>
 		)
