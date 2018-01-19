d515c9a1a590d02445c0afbaa55b3a10fef56c65
Alfredo <atakori@outlook.com>
2018-01-19 00:06:54 -0500

    added redux thunk middlewear to index.js

--- src/index.js
+++ src/index.js
@@ -4,11 +4,12 @@ import {Provider} from 'react-redux';
 import {createStore, applyMiddleware} from 'redux';
 import store from './store';
 import backlogReducer from './reducers'; 
+import reduxThunk from 'redux-thunk';
 
 import './index.css';
 import App from './components/App';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
 
 ReactDOM.render(
 	<Provider store={createStoreWithMiddleware(backlogReducer)}>
