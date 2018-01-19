90d04b96f2fe95fd61fc2ffdd496f54b9cb1a11b
Alfredo <atakori@outlook.com>
2018-01-19 00:02:08 -0500

    added index.js for actions folder

---
+++ src/actions/index.js
@@ -0,0 +1,9 @@
+export function loginUser({username, password}) {
+	//submit username and password to server
+
+	//if request is good, update state with authenticated user
+	//save JWT token
+	//redirect to route /dashboard
+
+	//if request is bad, show an error to the user
+}
