ec885c1ccb017e2e7655335203c317f7b795e3c7
Alfredo <atakori@outlook.com>
2018-01-20 19:44:43 -0500

    added requireAuth.js HOC

--- src/components/App.js
+++ src/components/App.js
@@ -8,6 +8,7 @@ import SignUp from './signup';
 import Dashboard from './dashboard';
 import {GameCollection} from './gameCollection';
 import {GamePage} from './gamePage';
+import RequireAuth from './requireAuth';
 
 class App extends Component {
   render() {
@@ -17,7 +18,7 @@ class App extends Component {
 	      		<Route exact path= '/' component= {LandingPage} />
 	      		<Route exact path= '/login' component= {Login}/>
 	      		<Route exact path= '/signup' component= {SignUp}/>
-	    		<Route exact path= '/dashboard' component= {Dashboard}/>
+	    		<Route exact path= '/dashboard' component= {RequireAuth(Dashboard)}/>
 	    		<Route exact path= '/mygamelibrary/:userID' component= {GameCollection} />
 	    		<Route exact path= '/gameInfo/:game' component= {GamePage}/>
 	    	</Switch>
