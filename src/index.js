21541d5ee747585a69e365cd30c947005f34d2bf
Alfredo <atakori@outlook.com>
2018-01-20 20:01:18 -0500

    modified index.js to auto auth users

--- src/index.js
+++ src/index.js
@@ -2,17 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
 import {createStore, applyMiddleware} from 'redux';
-import store from './store';
 import backlogReducer from './reducers'; 
 import reduxThunk from 'redux-thunk';
+import { AUTH_USER } from './actions/types'; 
 
 import './index.css';
 import App from './components/App';
 
 const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
+const store= createStoreWithMiddleware(backlogReducer);
+
+const token= localStorage.getItem('token');
+//if there is a token, then the user is signed in
+if(token) {
+	//update app state
+	store.dispatch({type: AUTH_USER});
+}
 
 ReactDOM.render(
-	<Provider store={createStoreWithMiddleware(backlogReducer)}>
+	<Provider store={store}>
 		<App />
 	</Provider>,
 	document.getElementById('root'));
