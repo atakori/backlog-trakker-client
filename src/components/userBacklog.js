30daf3601913413b40335f53232c6deff2f1e3bf
Alfredo <atakori@outlook.com>
2018-01-25 20:29:20 -0500

    added renderGameStatus() for userBacklog.js

--- src/components/userBacklog.js
+++ src/components/userBacklog.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Progress } from 'antd';
 
 export class ShowUserBacklog extends React.Component {
 	constructor(props) {
@@ -16,21 +17,35 @@ export class ShowUserBacklog extends React.Component {
 		console.log(transformedUrl);
 		return transformedUrl;
 	}
-/*
-	renderGameStatus(startStatus) {
-		if()
-	}*/
+
+	renderGameStatus(completedChapters, totalChapters) {
+		console.log(completedChapters);
+		console.log(totalChapters);
+		let percentComplete = ((completedChapters.length / totalChapters.length) *100)
+		if (percentComplete === 0) {
+			return (
+				<button className= "start_status btn btn-danger">NOT STARTED</button>
+				)
+		}
+		return (
+			<div className= "start_status">
+				<p> Perecentage complete</p>
+		        <Progress type= "circle" percent= {percentComplete.toFixed(2)}/>
+			</div>
+			)
+	}
+
 
 	renderUsersFullBacklog() {
 		// render full backlog from user's game collection
-		let games= this.props.gameCollection;
+		let games= this.props.gameCollection;	
 			let gameslist= games.map((game, index) => (
 			<div className= "game_info">
 	            <li className= "game" key= {index}>
 			  	<img src= {this.renderGameArtUrl(game.gameArtUrl)} alt="Game Box Art" className= "game_box_art"/>
             	<p className= "game_title"><a href= {`/gameInfo/${game.name.replace(/\s/g, "-")}`}>{game.name}</a></p>
             	</li>
-            	 <p className= "game_completion_status"> [Game Status goes here] </p>
+            	 <p className= "game_completion_status"> {this.renderGameStatus(game.completedChapters, game.gameChapters)}</p>
             </div>
 			))
 			return gameslist;
