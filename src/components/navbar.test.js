ae4ee3c21145bd8524b8d2c40f170d405b420b24
Alfredo <atakori@outlook.com>
2018-01-30 22:47:28 -0500

    completed navbar.test

--- src/components/navbar.test.js
+++ src/components/navbar.test.js
@@ -12,4 +12,15 @@ describe('<NavigationBar />', () => {
 		const dispatch = jest.fn();
 		shallow(<NavigationBar getCurrentUser= {() => dispatch}/>);
 	})
+	it('renders the searchbar if the user is logged in', () => {
+		const dispatch = jest.fn();
+		const wrapper= shallow(<NavigationBar authenticated= {true} currentUser= {'test_user'}getCurrentUser= {dispatch}/>)
+		const expectedValue = " My Collection "
+		expect(wrapper.find('li').at(0).text()).toEqual(expectedValue);
+	})
+	it('does not render the searchbar if the user is not logger in', () => {
+		const dispatch= jest.fn();
+		const wrapper= shallow(<NavigationBar currentUser= {'test_user'}getCurrentUser= {dispatch}/>)
+		expect(wrapper.find('li').at(0).text()).toEqual('Login')
+	})
 })
