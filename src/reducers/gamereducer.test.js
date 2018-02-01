1d0f06fa7b20a445df487f5712a7194641899669
Alfredo <atakori@outlook.com>
2018-01-31 22:16:27 -0500

    added send error test to gamereducers.test

--- src/reducers/gamereducer.test.js
+++ src/reducers/gamereducer.test.js
@@ -1,7 +1,7 @@
 import gameReducer from './gamereducer'
-import {LOADING_FINISHED} from '../actions/types'
+import {LOADING_FINISHED, SEND_ERROR} from '../actions/types'
 
-describe('Auth User', () => {
+describe('Loading Finished', () => {
 	it('should return a false loading state', () => {
 	let state= {
 		loading: true
@@ -9,6 +9,18 @@ describe('Auth User', () => {
 	state = gameReducer(state, {type: LOADING_FINISHED})
 	expect(state).toEqual({
 		loading: false
+	})
+	})
+})
+
+describe('Send Error', () => {
+	it('should return an error if something went wrong', () => {
+	let state= {
+		error: null
+		}
+	state = gameReducer(state, {type: SEND_ERROR, payload: "Error here"})
+	expect(state).toEqual({
+		error: "Error here"
 	})
 	})
 })
