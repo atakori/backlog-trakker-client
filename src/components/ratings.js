567636b7bc6861befe0e7ebe7ca9152a70467f8b
Alfredo <atakori@outlook.com>
2018-02-07 21:47:05 -0500

    changed ratings.js to export class to render scores correctly

--- src/components/ratings.js
+++ src/components/ratings.js
@@ -1,16 +1,47 @@
 import React from 'react';
 import { Progress } from 'antd';
+import LoadingScreen from './loading';
 
-export function Ratings(props) {
-	return (
-		<div className= "info_scores">
+export class Ratings extends React.Component {
+/*  constructor(props) {
+    super(props)
+  }*/
+
+  renderCriticScore(){
+    if(this.props.criticAvgRating === undefined) {
+      return (
+        <LoadingScreen />
+        )
+    } else {
+      return(
+      <Progress type= "circle" percent= {this.props.criticAvgRating.toFixed(2) * 10} format={percent => `${percent / 10}`}/>
+      )
+    }
+  }
+
+  renderUserScore(){
+    if(this.props.userAvgRating === undefined) {
+      return (
+        <LoadingScreen />
+        )
+    } else {
+      return (
+        <Progress type= "circle" percent= {this.props.userAvgRating.toFixed(2) * 10} format={percent => `${percent / 10}`} />
+        )
+    }
+  }
+
+  render() {
+	return (	
+    <div className= "info_scores">
           <h2 className= "info_rating_title"> Ratings</h2>
           <div className= "info_scores_container">
           	<p className= "critic_avg_score">Critic Score</p>
-          	<Progress type= "circle" percent= {props.criticAvgRating * 10} format={percent => `${percent / 10}`}/>
+            {this.renderCriticScore()}
           	<p className= "user_avg_score"> User Score</p>
-          	<Progress type= "circle" percent= {props.userAvgRating * 10} format={percent => `${percent / 10}`} />
+            {this.renderUserScore()}
           </div>
         </div>
 		)
+}
 }
